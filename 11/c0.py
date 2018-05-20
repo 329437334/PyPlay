d7d4d27087513d59e2782e9957ace9272e0e3fc9
mccRee <301063915@qq.com>
2018-05-20 10:38:51 +0800

    闭包

---
+++ 11/c0.py
@@ -0,0 +1,9 @@
+a = 10
+
+
+def f1(x):
+    return a*x*x
+
+
+f1(2)
+print(f1(2))
--- 11/c2.py
+++ 11/c2.py
@@ -1,15 +1,20 @@
 '''
-闭包
+闭包 的本质是 函数 + 它的环境变量
 '''
 
 # 函数中定义函数,外部无法调用局部函数
 # 把函数作为返回值返回
+
+
 def curve_pre():
     a = 25
+
     def curev(x):
-        print('This is a function')
         return a*x*x
     return curev
 
-fun = curve_pre()
-fun()
+
+a = 10
+
+f = curve_pre()
+print(f(2))
