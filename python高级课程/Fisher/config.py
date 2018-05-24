5b27586a795c2008516f58591becdeff035a93a1
马千里 <maqianli@ysten.com>
2018-05-24 15:45:09 +0800

    flask 配置文件与 if __name__ 用于生成环境的作用

---
+++ python高级课程/Fisher/config.py
@@ -0,0 +1,7 @@
+'''
+    create by mccree
+    配置文件
+'''
+
+#注意配置文件参数要大写不然会被忽略
+DEBUG = True
--- python高级课程/Fisher/fisher.py
+++ python高级课程/Fisher/fisher.py
@@ -3,14 +3,19 @@
 '''
 from flask import Flask
 
+
 app = Flask(__name__)
+#载入配置文件
+app.config.from_object('config')
+print(app.config['DEBUG'])
 
-
-@app.route('/hello/')
+@app.route('/start')
 def hello():
-    #基于函数的视图
-    #基于类的视图(即插视图)
+    #基于函数的视图,推荐
     return 'hello Qiyue'
 
+#另一种路由注册办法,基于类的视图(即插视图)
 
-app.run()
+if __name__ == '__main__':
+    # 生成环境一般会使用 nginx+uwsgi 由uwsgi来加载fisher.py,所以app.run不会被执行
+    app.run(host='0.0.0.0',debug=app.config['DEBUG'],port=81)
