827c0f37b39859eb78cf979ff4a09a8c14ffd884
马千里 <maqianli@ysten.com>
2018-05-25 17:40:38 +0800

    4-1 app与蓝图的初始化

---
+++ python高级课程/Fisher/app/__init__.py
@@ -0,0 +1,11 @@
+'''
+    Create by MccRee
+'''
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    # 载入配置文件
+    app.config.from_object('config')
+    return app
--- python高级课程/Fisher/fisher.py
+++ python高级课程/Fisher/fisher.py
@@ -2,26 +2,9 @@
     Create by mccree
 '''
 
-from flask import Flask, jsonify
-from helper import is_isbn_or_key
-from yushu_book import YuShuBook
+from app import create_app
 
-app = Flask(__name__)
-# 载入配置文件
-app.config.from_object('config')
-print(app.config['DEBUG'])
-
-
-@app.route('/book/search/<q>/<page>')
-def search(q, page):
-    isbn_or_key = is_isbn_or_key(q)
-
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
-    # return json.dumps(result), 200, {'content-type':'application/json'}
+app = create_app()
 
 
 if __name__ == '__main__':
