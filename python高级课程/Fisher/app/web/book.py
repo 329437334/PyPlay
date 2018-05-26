877ffd2b6fe54a3376107b9f94866a7e9e55f5c8
MccRee <301063915@qq.com>
2018-05-26 11:10:38 +0800

    4-4使用flask自带的Request来接受 get方法的?后的参数

--- python高级课程/Fisher/app/web/book.py
+++ python高级课程/Fisher/app/web/book.py
@@ -2,15 +2,23 @@
     Create by MccRee
     #蓝图 blueprint
 '''
-from flask import jsonify
+from flask import jsonify, request
 
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 from .blueprint import web
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search')
+def search():
+    '''
+    Request类实例化后用于？接参数
+    :param q:
+    :param page:
+    :return:
+    '''
+    q = request.args['q']
+    page = request.args['page']
     isbn_or_key = is_isbn_or_key(q)
 
     if isbn_or_key == 'isbn':
