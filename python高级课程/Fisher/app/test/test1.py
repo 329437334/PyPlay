857ced91c6d904b436de7e1a9763dfe9e5059364
马千里 <maqianli@ysten.com>
2018-05-30 17:58:36 +0800

    6-10 LocalStack 线程隔离

--- python高级课程/Fisher/app/test/test1.py
+++ python高级课程/Fisher/app/test/test1.py
@@ -4,10 +4,14 @@
 
 import threading, time
 
+from werkzeug.local import Local
+
+
 def download():
     print('i am thread')
     t = threading.current_thread()
     print(t.getName())
+
 
 t = threading.current_thread()
 print(t.getName())
@@ -19,5 +23,30 @@ new_t.start()
 
 # flask web框架
 # 请求             线程
-# 10个请求     webserver开启
+# 多个请求     webserver开启
 # Java PHP Nginx Apache Tomcat IIS
+
+# werkzeug Local 线程隔离 字典结构
+# LocalStack Local 字典
+# Local使用字典的方式实现线程隔离
+# LocalStack封装了Local作为自己的属性实现了栈结构
+
+class A:
+    b = 1
+
+
+# my_obj = A()
+# 使用Local对象来线程隔离,原理就是用线程id作为字典key
+my_obj = Local()
+my_obj.b = 1
+
+
+def worker():
+    my_obj.b = 2
+    print('in new thread b is :' + str(my_obj.b))
+
+
+new_t2 = threading.Thread(target=worker, name='qy_thread')
+new_t2.start()
+time.sleep(1)
+print('in main thread b is :' + str(my_obj.b))
