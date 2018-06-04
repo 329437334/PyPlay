57d9bc5d0348accdd56c4fa4ab530e460ab5346f
马千里 <maqianli@ysten.com>
2018-06-04 17:19:31 +0800

    9-1 将搜索结果页中的 作者说明内容加上  book.intros函数修改

--- python高级课程/Fisher/app/view_models/book.py
+++ python高级课程/Fisher/app/view_models/book.py
@@ -2,6 +2,7 @@
     Create by MccRee
 '''
 
+
 class BookViewModel:
     # 描述特征(类变量, 实例变量)
     # 行为(方法)
@@ -14,10 +15,20 @@ class BookViewModel:
         self.summary = book['summary']
         self.pages = book['pages']
 
+    #用属性访问的方式来调用函数,类似get方法
+    @property
+    def intro(self):
+        # filter(过滤规则,过滤对象[])
+        intros = filter(lambda x: True if x else False,
+                        [self.author, self.publisher, self.price])
+        return '/'.join(intros)
+
+
 class BookCollection:
     '''
     一组bookViewModel
     '''
+
     def __init__(self):
         self.total = 0
         self.books = []
@@ -27,4 +38,3 @@ class BookCollection:
         self.total = yushu_book.total
         self.keyword = keyword
         self.books = [BookViewModel(book) for book in yushu_book.books]
-
