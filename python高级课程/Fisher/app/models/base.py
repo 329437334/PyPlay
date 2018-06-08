f2bc3fe442044204b30375b5cb94e59ac8bc12fe
马千里 <maqianli@ysten.com>
2018-06-08 18:06:19 +0800

    update base.py

--- python高级课程/Fisher/app/models/base.py
+++ python高级课程/Fisher/app/models/base.py
@@ -1,14 +1,14 @@
 '''
     Create by MccRee
 '''
+import datetime
 from contextlib import contextmanager
 
 from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
 
 from sqlalchemy import SmallInteger, Column, Integer
 
-# 这里在引入SQLAlchemy时,因为子类想要用SQLAlchemy名字,所以父类引入时就as _SQLAlchemy
-db = _SQLAlchemy()
+
 
 
 class SQLAlchemy(_SQLAlchemy):
@@ -23,11 +23,17 @@ class SQLAlchemy(_SQLAlchemy):
             raise e
 
 
+# 这里在引入SQLAlchemy时,因为子类想要用SQLAlchemy名字,所以父类引入时就as _SQLAlchemy
+db = SQLAlchemy()
+
 class Base(db.Model):
     # __abstract__ = True 这样就不会真实创建表
     __abstract__ = True
     create_time = Column('create_time', Integer)
     status = Column(SmallInteger, default=1)
+
+    def __init__(self):
+        self.create_time = int(datetime.datetime.now().timestamp())
 
     def set_attrs(self, attrs_dict):
         '''
--- python高级课程/Fisher/app/web/gift.py
+++ python高级课程/Fisher/app/web/gift.py
@@ -1,7 +1,7 @@
 '''
     Create by MccRee
 '''
-from flask import current_app, flash
+from flask import current_app, flash, redirect, url_for
 
 from app import db
 from app.models.gift import Gift
@@ -36,7 +36,7 @@ def save_to_gifts(isbn):
             # current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
     else:
         flash('这本书已添加至你的赠送清单或已存在于你的心愿清单，请不要重复添加')
-    # return redirect(url_for('web.book_detail', isbn=isbn))
+    return redirect(url_for('web.book_detail', isbn=isbn))
 
 @web.route('/gifts/<gid>/redraw')
 def redraw_from_gifts(gid):
