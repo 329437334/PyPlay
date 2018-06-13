593d7aae03835f9d6cbaa36b8767f7f39ff43037
马千里 <maqianli@ysten.com>
2018-06-13 17:31:53 +0800

    首页查不到

--- python高级课程/Fisher/app/web/main.py
+++ python高级课程/Fisher/app/web/main.py
@@ -1,7 +1,10 @@
 '''
     Create by MccRee
 '''
+from flask import render_template
+
 from app.models.gift import Gift
+from app.view_models.book import BookViewModel
 from .blueprint import web
 
 
@@ -9,6 +12,7 @@ from .blueprint import web
 def index():
     recent_gifts = Gift.recent()
     books = [BookViewModel(gift.book) for gift in recent_gifts]
+    return render_template('index.html', recent=books)
 
 
 @web.route('/personal')
