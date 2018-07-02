8aee58754b192bb568d54890bb78e5020aaf4aa0
马千里 <maqianli@ysten.com>
2018-07-02 15:37:30 +0800

    3-2 REST风格讲解

--- RestfulAPI课程/ginger/app/api/v1/user.py
+++ RestfulAPI课程/ginger/app/api/v1/user.py
@@ -3,12 +3,31 @@
 '''
 from flask import Blueprint
 
-#使用蓝图
+# 使用蓝图
 from app.libs.redprint import Redprint
 
 api = Redprint('user')
 
-@api.route('/get')
+
+@api.route('', methods=['GET'])
 def get_user():
+    # REST 接口粒度比较粗
+    # HTTP请求数量大幅增加
+    # REST最适合做开放性API不掺杂业务逻辑
+    # 遵守REST规范, 根据业务灵活变通
     return 'I am qy'
 
+
+@api.route('', methods=['PUT'])
+def update_user():
+    return 'update qy'
+
+
+@api.route('', methods=['DELETE'])
+def delete_user():
+    return 'delete qy'
+
+
+@api.route('', methods=['POST'])
+def create_user():
+    pass
