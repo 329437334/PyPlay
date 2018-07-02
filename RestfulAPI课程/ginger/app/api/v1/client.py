697ca5782a5cb8109fd150bd4885a923ea8afac1
马千里 <maqianli@ysten.com>
2018-07-02 16:22:14 +0800

    4-3 处理不同客户端的注册方案

--- RestfulAPI课程/ginger/app/api/v1/client.py
+++ RestfulAPI课程/ginger/app/api/v1/client.py
@@ -1,14 +1,34 @@
 '''
     Create by MccRee
 '''
+from flask import request
+
+from app.libs.enums import ClientTypeEnum
 from app.libs.redprint import Redprint
+from app.validators.forms import ClientForm
 
 api = Redprint('client')
 
 
-@api.route('/register')
+@api.route('/register', methods=['POST'])
 def create_clinet():
     # 注册
     # 参数 校验 接收参数
     # WTForms
+    # 表单提交  网页
+    # JSON提交 APP
+
+    data = request.json
+    form = ClientForm(data=data)
+    if form.validate():
+        #switch不同类型客户端注册不同
+        promise = {
+            ClientTypeEnum.USER_EMAIL:__register_user_by_email,
+        }
+
+
+    pass
+
+
+def __register_user_by_email():
     pass
