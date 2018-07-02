7adb31bf80625e0b0fa35fc49c319a75270e187e
马千里 <maqianli@ysten.com>
2018-07-02 15:00:39 +0800

    3-1 REST API设计风格

--- RestfulAPI课程/ginger/app/api/v1/book.py
+++ RestfulAPI课程/ginger/app/api/v1/book.py
@@ -11,10 +11,11 @@ api = Redprint('book')
 #使用蓝图分离视图函数,是存在缺陷的,蓝图的本意是用来区分模块的,而不是只用来分离视图函数
 #构建自定义对象redprint
 
-@api.route('/get')
+@api.route('/get', methods=['GET'])
 def get_book():
+    #设计API时尽量不需要包含动词
     return 'get book'
 
-@api.route('/create')
+@api.route('/create', methods=['POST'])
 def create_book():
     return 'create book'
