06bf2ca1e744876a36537b1d29fb31bddfa55918
mccRee <301063915@qq.com>
2019-08-13 14:43:22 +0800

    U MixPlay函数混淆测试

---
+++ MixPlay/MixPlay/codeObfuscation.h
@@ -0,0 +1,7 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Tue Aug 13 14:00:43 CST 2019
+#define helloWorld smjNgcHLFDFRpwtD
+#define checkLogin dwmHIdmbEauEDLhY
+#define changeColor finmAhrTbtnrklYm
+#endif
