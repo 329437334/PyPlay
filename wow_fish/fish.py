3a11123d506111a0a9aaed34977fa8ded80d27a2
mccree <301063915@qq.com>
2019-09-02 19:04:13 +0800

    no message

--- wow_fish/fish.py
+++ wow_fish/fish.py
@@ -5,3 +5,16 @@ import time
 while True:
     time.sleep(30)
     pyautogui.press('1')
+
+    pyautogui.keyDown('w')
+    time.sleep(1)
+    pyautogui.keyUp('w')
+    pyautogui.keyDown('s')
+    time.sleep(1)
+    pyautogui.keyUp('s')
+    pyautogui.keyDown('a')
+    time.sleep(1)
+    pyautogui.keyUp('a')
+    pyautogui.keyDown('d')
+    time.sleep(1)
+    pyautogui.keyUp('d')
