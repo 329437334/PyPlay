d2bf7c386cf3913b5be1532ec0de1024fd75a35a
mccree <301063915@qq.com>
2019-09-02 21:45:55 +0800

    U  安装python opencv依赖 打印截图路径

--- wow_fish/fish.py
+++ wow_fish/fish.py
@@ -1,20 +1,34 @@
+import cv2 as cv
 import pyautogui
 import os
 import time
+import glob
 
-while True:
-    time.sleep(30)
-    pyautogui.press('1')
+# WOW截图文件夹路径
+scrnShotPath = r'/Users/mccree/Desktop/Blizzard/World of Warcraft/_classic_/Screenshots/*.jpg'
+# 拿到最新的截图路径
+imgSrc = glob.glob(scrnShotPath)[-1]
+print("截图路径:{}".format(imgSrc))
+src = cv.imread(imgSrc)
 
-    pyautogui.keyDown('w')
-    time.sleep(1)
-    pyautogui.keyUp('w')
-    pyautogui.keyDown('s')
-    time.sleep(1)
-    pyautogui.keyUp('s')
-    pyautogui.keyDown('a')
-    time.sleep(1)
-    pyautogui.keyUp('a')
-    pyautogui.keyDown('d')
-    time.sleep(1)
-    pyautogui.keyUp('d')
+
+
+
+
+
+# while True:
+#     time.sleep(30)
+#     pyautogui.press('1')
+#
+#     pyautogui.keyDown('w')
+#     time.sleep(1)
+#     pyautogui.keyUp('w')
+#     pyautogui.keyDown('s')
+#     time.sleep(1)
+#     pyautogui.keyUp('s')
+#     pyautogui.keyDown('a')
+#     time.sleep(1)
+#     pyautogui.keyUp('a')
+#     pyautogui.keyDown('d')
+#     time.sleep(1)
+#     pyautogui.keyUp('d')
