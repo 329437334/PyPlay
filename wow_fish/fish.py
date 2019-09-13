f63e78172fe6fe4b3b70c77524e8d078cf94d73b
mccree <301063915@qq.com>
2019-09-13 22:07:20 +0800

    U  fish Release

--- wow_fish/fish.py
+++ wow_fish/fish.py
@@ -1,15 +1,10 @@
 import cv2 as cv
 import pyautogui
-import os
-import time, threading
-import glob
-import asyncio
+import time
 
-# WOW截图文件夹路径
-fishImgPath = r'/Users/mccree/Desktop/Blizzard/fishImg/fishImg.png'
+# 浮漂图片
+fishImgPath = r'fishImg.png'
 # 拿到最新的截图路径
-# scrnShotPath = r'wowFish.png'
-# imgSrc = glob.glob(scrnShotPath)[-1]
 imgSrc = r'wowFish.png'
 print("截图路径:{}".format(imgSrc))
 
@@ -40,14 +35,6 @@ def findFishFloat():
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
     top_left = min_loc  # 左上角的位置
     bottom_right = (top_left[0] + w, top_left[1] + h)  # 右下角的位置
-    # 在原图上画矩形
-
-    # cv.rectangle(imgBig, top_left, bottom_right, (0, 0, 255), 2)
-    # 显示原图和处理后的图像
-    # cv.namedWindow('wowFishImage', cv.WINDOW_FULLSCREEN)
-    # cv.imshow("wowFishImage", imgBig)
-    # cv.waitKey(0)11
-    # cv.destroyAllWindows()
 
     print('检测结果{},{}'.format(top_left, bottom_right))
     x = (top_left[0] + bottom_right[0]) / 2
@@ -61,10 +48,6 @@ def moveMouse(x,y):
     pyautogui.moveTo(x, y,duration=0.25)
 
 def stat():
-    # t = threading.Thread(target=printPiexl, name='Piexl')
-    # t.start()
-    # t.join()
-
     while True:
         pyautogui.press('1')
         time.sleep(2)
@@ -72,20 +55,10 @@ def stat():
         x, y = findFishFloat()
         moveMouse(x / 2, y / 2)
 
-        # t = threading.Thread(target=printPiexl, name='Piexl')
-        # t.start()
-        # t.join()
 
         time.sleep(8)
         pyautogui.rightClick()
-        # time.sleep(0.5)
 
-        # pyautogui.keyDown('A')
-        # time.sleep(1)
-        # pyautogui.keyUp('A')
-        # pyautogui.keyDown('D')
-        # time.sleep(1)
-        # pyautogui.keyUp('D')
 
 def printPiexl():
     c = 0
@@ -99,7 +72,3 @@ def printPiexl():
 
 time.sleep(3)
 stat()
-
-# print('thread {} is start'.format(threading.current_thread().name))
-
-# print('thread {} is end'.format(threading.current_thread().name))
